e036d288483c9048f7a4ee3d13360a2ff7f93acc
Mohamed Hussein <mfarrag19@gmail.com>
2024-10-19 11:19:03 +0300

    Buttons for onBoarding to Home Fragment

--- app/src/main/java/com/example/wiz_cast/OnBoardingScreen/OnBoardingFirstFragment.kt
+++ app/src/main/java/com/example/wiz_cast/OnBoardingScreen/OnBoardingFirstFragment.kt
@@ -5,18 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.example.wiz_cast.R
+import com.example.wiz_cast.databinding.FragmentOnBoardingFirstBinding
 
 
 class OnBoardingFirstFragment : Fragment() {
 
+    lateinit var binding: FragmentOnBoardingFirstBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_boarding_first, container, false)
+        binding = FragmentOnBoardingFirstBinding.inflate(layoutInflater, container, false)
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.view_pager)
+
+        binding.tvNext.setOnClickListener{
+            viewPager?.currentItem = 1 // move to the next fragment
+        }
+
+
+        return binding.root
     }
 
 
